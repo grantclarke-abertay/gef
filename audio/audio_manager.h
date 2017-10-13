056c6c34e16f3156c2d03c6cba99e6a88c221a93
Grant Clarke <g.clarke@abertay.ac.uk>
2017-10-13 06:55:06 +0100

    added virtual function declarations to audio manager to provide more control of audio output

--- audio/audio_manager.h
+++ audio/audio_manager.h
@@ -7,6 +7,18 @@ namespace gef
 {
 	class Platform;
 
+	struct VolumeInfo
+	{
+		float volume;
+		float pan;
+
+		VolumeInfo() :
+			volume(1.0f),
+			pan(0.0f)
+		{
+		}
+	};
+
 	class AudioManager
 	{
 	public:
@@ -22,6 +34,17 @@ namespace gef
 		virtual Int32 StopMusic() = 0;
 		virtual Int32 PlaySample(const Int32 sample_index, const bool looping = false) = 0;
 		virtual Int32 StopPlayingSampleVoice(const Int32 voice_index) = 0;
+
+		virtual Int32 SetSamplePitch(const Int32 voice_index, float pitch) = 0;
+		virtual Int32 SetMusicPitch(float pitch) = 0;
+		virtual Int32 GetSampleVoiceVolumeInfo(const Int32 voice_index, struct VolumeInfo& volume_info) = 0;
+		virtual Int32 SetSampleVoiceVolumeInfo(const Int32 voice_index, const struct VolumeInfo& volume_info) = 0;
+		virtual Int32 GetMusicVolumeInfo(struct VolumeInfo& volume_info) = 0;
+		virtual Int32 SetMusicVolumeInfo(const struct VolumeInfo& volume_info) = 0;
+		virtual Int32 SetMasterVolume(float volume) = 0;
+
+		virtual bool sample_voice_playing(const UInt32 voice_index) = 0;
+		virtual bool sample_voice_looping(const UInt32 voice_index) = 0;
 
 		static AudioManager* Create();
 	protected:
