14a19a4aeed4b45a8dc1929a2eedd2e8d85e742a
grantclarke-abertay <g.clarke@abertay.ac.uk>
2018-02-21 08:48:23 +0000

    integrate local imgui files into cmaklists.txt Moved FPS display position

--- samples/assimp_viewer/assimp_viewer.cpp
+++ samples/assimp_viewer/assimp_viewer.cpp
@@ -621,7 +621,7 @@ void AssimpViewer::DrawHUD()
 	if(font_)
 	{
 		// display frame rate
-		font_->RenderText(sprite_renderer_, gef::Vector4(850.0f, 510.0f, -0.9f), 1.0f, 0xffffffff, gef::TJ_LEFT, "FPS: %.1f", fps_);
+		font_->RenderText(sprite_renderer_, gef::Vector4(1170.0f, 686.0f, -0.9f), 1.0f, 0xffffffff, gef::TJ_LEFT, "FPS: %.1f", fps_);
 	}
 }
 
